a36d295eb08144703a674a0bfe13439d1f78e4e2
Jonathan Jesús González <gonzalezjonathanjesus@gmail.com>
2019-03-08 12:40:29 -0300

    Changed ->length = count(->length) to ->length++

--- examen/test.php
+++ examen/test.php
@@ -6,23 +6,18 @@ function isStraight($hand) {
         'cards' => $hand,
         'length' => count($hand)
     );
-
     if($hand->length >= $hand->straight  && $hand->length <= $hand->maxCardsPerHand) {
-        if ( in_array(14, $hand->cards) ) { 
-            array_push($hand->cards, 1);
-            $hand->length = count($hand->cards);
-        }
-
         sort($hand->cards);
-
+        if ($hand->cards[$hand->length - 1] == 14 ) { 
+            array_unshift($hand->cards, 1);
+            $hand->length++;
+        }
         $iterations = $hand->length - $hand->straight;
-
         for($i = 0; $i <= $iterations; $i++ ) {
             if($hand->cards[$i + 4] == ($hand->cards[$i] + 4)) {
                 return true;
             }
         }
-
         return false;
     } else {
         return false;
